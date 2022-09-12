64fbbcb0cdace484538197ca0131f332bd14249b
RMITProgrammingClub <programming.club@rmit.edu.au>
2022-09-12 18:54:00 +1000

    init arkanoid

--- src/config.jsx
+++ src/config.jsx
@@ -1,10 +1,9 @@
 import Head from 'next/head'
 
-const titleDefault = 'React Three Next Starter'
-const url = 'https://react-three-next.vercel.app/'
-const description =
-  'The easiest and fastest way to create a 3D website using React Three Fiber and NextJS'
-const author = 'Author'
+const titleDefault = 'Arkanoids'
+const url = 'https://championsacademyinfo.com.au'
+const description = 'A block breaker game'
+const author = ''
 
 const Header = ({ title = titleDefault }) => {
   return (
